753f1029d568f0b2130db1f78625346fef95e184
@404. <87565477+ERR0H@users.noreply.github.com>
2025-06-10 22:19:44 +0000

    them du lieu orders va order_details

--- db.js
+++ db.js
@@ -33,6 +33,62 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
 */
 
 /*
+// Tạo bảng orders
+db.run(`CREATE TABLE IF NOT EXISTS orders (
+    id_order TEXT PRIMARY KEY,
+    sdt TEXT,
+    order_date TEXT,
+    total REAL,
+    status TEXT,
+
+    FOREIGN KEY (sdt) REFERENCES users(sdt)
+)`);
+// doc file JSON
+const rawData = fs.readFileSync('./data/orders.json');
+const orders = JSON.parse(rawData);
+// Chèn dữ liệu vào database từ file JSON
+orders.forEach(order => {
+    db.run('INSERT INTO orders (id_order, sdt, order_date, total, status) VALUES (?, ?, ?, ?, ?)', 
+    [order.id_order, order.sdt, order.order_date, order.total, order.status], 
+    function(err) {
+        if (err) {
+            console.error('Lỗi:', err.message);
+        } else {
+            console.log(`Thêm đơn hàng thành công với ID: ${this.lastID}`);
+        }
+    });
+});
+*/
+
+/*
+// Tạo bảng order_details
+db.run(`CREATE TABLE IF NOT EXISTS order_details (
+    id TEXT,
+    id_order TEXT,
+    quantity INTEGER,
+    price REAL,
+
+    FOREIGN KEY (id_order) REFERENCES orders(id_order),
+    FOREIGN KEY (id) REFERENCES products(id)
+)`);
+
+// doc file JSON
+const rawData = fs.readFileSync('./data/order_details.json');
+const orderDetails = JSON.parse(rawData);
+// Chèn dữ liệu vào database từ file JSON
+orderDetails.forEach(detail => {
+    db.run('INSERT INTO order_details (id, id_order, quantity, price) VALUES (?, ?, ?, ?)', 
+    [detail.id, detail.id_order, detail.quantity, detail.price], 
+    function(err) {
+        if (err) {
+            console.error('Lỗi:', err.message);
+        } else {
+            console.log(`Thêm chi tiết đơn hàng thành công với ID: ${this.lastID}`);
+        }
+    });
+});
+*/
+/*
 // Đọc file JSON
 const rawData = fs.readFileSync('./data/products.json');
 const products = JSON.parse(rawData);
@@ -70,7 +126,7 @@ customers.forEach(customer => {
 */
 
 
-db.all("SELECT * FROM products", [], (err, rows) => {
+db.all("SELECT * FROM order_details", [], (err, rows) => {
     if (err) {
         console.error('Lỗi:', err.message);
     } else {
